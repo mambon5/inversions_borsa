00cbbbf41918df5041202d2c44424cc30d51c97b
mambon5 <monantoniet@gmail.com>
2023-04-22 11:51:46 +0200

    added a few basic plotting functions on historical open price of yfinance

--- finy.py
+++ finy.py
@@ -1,4 +1,8 @@
 import yfinance as yfin
+import pandas_datareader as pdr
+import pandas as pd
+import datetime as dt
+import matplotlib.pyplot as plt
 
 # how to get data from yahoo.
 # check this tutorial > https://www.geeksforgeeks.org/get-financial-data-from-yahoo-finance-with-python/
@@ -17,3 +21,68 @@ def main():
     for tick in ticks:
         stock = yfin.Ticker(tick)
         print("stock: {}, last price: {}".format(stock.info["longName"], stock.fast_info.last_price ))
+
+
+def historic_yfin(ticker, period="7d", interval="1m", start=None, end=None):
+    """
+    Getting historical stock data using the yfinance library.
+        - Remember we now use this aapl ticker object for almost everything- calling various methods on it.
+        - To get the historical data we want to use the history() method, which is the most 'complicated' method in the yfinance library.
+        - Check this guide on how to use it > https://algotrading101.com/learn/yfinance-guide/ 
+
+        :param start: must be of format: "2020-06-02"
+        :param end: must be of format: "2020-06-02"
+        :param period: must be of format '1d', '5d', '1mo', '3mo', '6mo', '1y', '2y', '5y', '10y', 'ytd', 'max'
+        :param interval: valid intervals include: '1m', '2m', '5m', '15m', '30m', '60m', '90m', '1h', '1d', '5d', '1wk', '1mo', '3mo'
+
+    """
+
+    # 
+    stock= yfin.Ticker(ticker)
+    if start == None :
+        stock_historical = stock.history(period=period, interval=interval)
+    else:
+        stock_historical = stock.history(start=start, end=end, interval=interval)
+
+    print(stock_historical)
+    plot_open(stock_historical)
+    return stock_historical
+
+
+def historic_pandas():
+    """
+    historical yahoo finance data.
+    """
+    ticker = "AAPL"
+    start = dt.datetime(2019, 1, 1)
+    end = dt.datetime(2020, 12, 31)
+    
+    data = pdr.get_data_yahoo(ticker, start, end)
+    print(data)
+
+def plot_values(value, time=None,  title="", pandas_values=False):
+    if pandas_values:
+            plt.plot(value)
+    else:
+        if len(time) == len(value):        
+            # plotting the points 
+            plt.plot(time,value)        
+        else:
+            print("error, time and values have different dimensions!")
+    # naming the x axis
+    plt.xlabel('t - time')
+    # naming the y axis
+    plt.ylabel('y - values')
+    
+    # giving a title to my graph
+    plt.title('Stock values {}'.format(title))
+    
+    # function to show the plot
+    plt.show()
+
+    
+
+def plot_open(historic):
+    y = pd.to_numeric(historic["Open"])    
+    plot_values(y, title="Open values", pandas_values=True)
+    
